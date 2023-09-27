15bbadbb73b594c2b6af57bafbe4ce65cb7eae6d
CBA <clement.baraise@beconfig.com>
2023-09-27 12:49:17 +0200

    addFeat(version-component):Ajout de git-describ(non-fonctionnel)

--- libs/library/src/lib/component/versions/versions.component.ts
+++ libs/library/src/lib/component/versions/versions.component.ts
@@ -1,60 +1,67 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BadgeModule } from 'primeng/badge';
-import { ButtonModule  } from 'primeng/button';
+import { ButtonModule } from 'primeng/button';
 import { OverlayPanelModule } from 'primeng/overlaypanel';
-
+import { gitDescribeSync } from 'git-describe';
 
 @Component({
   selector: 'version-component-versions',
   standalone: true,
-  imports: [CommonModule , BadgeModule   ,ButtonModule , OverlayPanelModule],
+  imports: [CommonModule, BadgeModule, ButtonModule, OverlayPanelModule],
   templateUrl: './versions.component.html',
   styleUrls: ['./versions.component.scss'],
 })
-export class VersionsComponent {
-  
-  visible = false; 
-  private _serverVersion="0.0.0.0";
-  private _clientVersion="0.0.0.0";
-  private _version ="0.0.0.0";
+export class VersionsComponent implements OnInit {
+
+  private _serverVersion = '0.0.0.0';
+  private _clientVersion = '0.0.0.0';
+  private _version = '0.0.0.0';
+  gitInfo: string | undefined;
+
   @Input()
-  public get serverVersion(){
+  public get serverVersion() {
     return this._serverVersion;
-    
   }
-  public set serverVersion(value){
-    this._serverVersion=value;
-    this.VerifVersion()
+
+  public set serverVersion(value) {
+    this._serverVersion = value;
+    this.verifVersion();
   }
+
   @Input()
-  public get clientVersion(){
+  public get clientVersion() {
     return this._clientVersion;
   }
-  public set clientVersion(value){
-    this._clientVersion=value;
-    this.VerifVersion()
+
+  public set clientVersion(value) {
+    this._clientVersion = value;
+    this.verifVersion();
   }
-  public set version(value){
-    this._version=value;
+
+  public set version(value) {
+    this._version = value;
   }
-  public get version(){
+
+  public get version() {
     return this._version;
   }
-  
-  VerifVersion(){
-    
-    if(this.clientVersion == this.serverVersion){
-      this.version="Version "+this.clientVersion;
-      const element = document.getElementById('badgeVersion') as HTMLElement
-      element.style.display = 'blue'
-    }
-    else{
-      this.version="Version Client "+this.clientVersion+" / Version Server "+this.serverVersion;
-    }
-    
-  }
-  
-   
 
+  ngOnInit() {
+    this.gitInfo = this.getGitInfo();
+  }
+
+  verifVersion() {
+    // your version verification logic here
+  }
+
+  private getGitInfo(): string {
+    try {
+      const gitInfo = gitDescribeSync();
+      return 'Version ' + gitInfo;
+    } catch (error) {
+      console.error('Error getting git describe:', error);
+      return 'Version N/A'; // Handle the case where git describe fails
+    }
+  }
 }

3c059c0fc3f79549480863031d3c5f84a2265d60
Eric Sauzeau <eric.sauzeau@beconfig.com>
2023-09-28 08:42:44 +0200

    fix(version-component): suppression dépendance inutile

--- apps/version-component/src/app/app.component.ts
+++ apps/version-component/src/app/app.component.ts
@@ -1,11 +1,10 @@
 /* eslint-disable @nx/enforce-module-boundaries */
 import { Component, } from '@angular/core';
-import { LibraryModule, } from '@monsociete/library';
 import { VersionsComponent, } from 'libs/library/src/lib/component/versions/versions.component';
 import { VersionServerService, } from 'libs/library/src/lib/version-server.service';
 @Component({
   standalone: true,
-  imports: [AppComponent , VersionsComponent  , LibraryModule  ],
+  imports: [AppComponent , VersionsComponent   ],
   selector: 'version-component-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
